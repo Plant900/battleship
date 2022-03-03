65ca104549703bf60e954edb99ffe95de7ade86a
PlanB900 <rythodir@gmail.com>
2022-03-03 15:41:39 -0700

    Grammar edit

--- src/createGameboard.js
+++ src/createGameboard.js
@@ -2,13 +2,19 @@ export { createGameboard }
 import { createShip } from "./createShip.js"
 
 function createGameboard() {
+	// this board displays all ships currently on the board
 	let board = Array.apply(null, new Array(10)).map(function () {
-		return new Array(10)
+		return [...Array(10)]
+	})
+
+	// this board only displays hits/misses for purposes of rendering
+	let hiddenBoard = Array.apply(null, new Array(10)).map(function () {
+		return [...Array(10)]
 	})
 
 	let ships = []
 
-	function placeShip(x, y, length, direction) {
+	function placeShip(y, x, length, direction) {
 		let newShip = createShip(length)
 		ships.push(newShip)
 
@@ -16,17 +22,18 @@ function createGameboard() {
 		//as well as a number indicating which 'spot' on the ship it is
 		for (let i = 0; i < length; i++) {
 			if (direction == "h") {
-				board[x][y + i] = [newShip.name, i]
+				board[y][x + i] = [newShip.name, i]
 			} else if (direction == "v") {
-				board[x + i][y] = [newShip.name, i]
+				board[y + i][x] = [newShip.name, i]
 			}
 		}
 	}
 
-	function receiveAttack(x, y) {
-		let attack = board[x][y]
+	function receiveAttack(y, x) {
+		let attack = board[y][x]
 		if (attack === undefined) {
-			board[x][y] = "miss"
+			board[y][x] = "miss"
+			hiddenBoard[y][x] = "miss"
 		} else {
 			// since attack did hit a ship, find the ship in the ships array with the matching name
 			let target = ships.find((ship) => {
@@ -34,6 +41,7 @@ function createGameboard() {
 			})
 
 			target.hit(attack[1])
+			hiddenBoard[y][x] = "hit"
 		}
 	}
 
@@ -45,6 +53,7 @@ function createGameboard() {
 
 	return {
 		board,
+		hiddenBoard,
 		ships,
 		placeShip,
 		receiveAttack,
